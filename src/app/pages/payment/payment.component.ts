62b6f77ed165b8da2e8ac69f2982d1a6a62bb628
Benjamin mitton <benjamin.mitton@avisto.com>
2025-09-05 16:59:35 +0200

    add payment page

--- src/app/pages/payment/payment.component.ts
+++ src/app/pages/payment/payment.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HeaderImageComponent } from '../../core/components/header-image/header-image.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-payment',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './payment.component.html',
-  styleUrl: './payment.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-payment',
+    standalone: true,
+    imports: [CommonModule, HeaderImageComponent, RouterModule],
+    templateUrl: './payment.component.html',
+    styleUrl: './payment.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PaymentComponent {}
