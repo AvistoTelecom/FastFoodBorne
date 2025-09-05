77c69daa11bbe2e115408c3b4436f2a42407f114
Benjamin mitton <benjamin.mitton@avisto.com>
2025-09-05 16:34:23 +0200

    splash screen

--- src/app/pages/location/location.component.ts
+++ src/app/pages/location/location.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderImageComponent } from '../../core/components/header-image/header-image.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-location',
     standalone: true,
-    imports: [CommonModule, HeaderImageComponent],
+    imports: [CommonModule, RouterModule, HeaderImageComponent],
     templateUrl: './location.component.html',
     styleUrl: './location.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/pages/splash-screen/splash-screen.component.ts
+++ src/app/pages/splash-screen/splash-screen.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-splash-screen',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './splash-screen.component.html',
-  styleUrl: './splash-screen.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-splash-screen',
+    standalone: true,
+    imports: [CommonModule, RouterLink],
+    templateUrl: './splash-screen.component.html',
+    styleUrl: './splash-screen.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SplashScreenComponent {}
