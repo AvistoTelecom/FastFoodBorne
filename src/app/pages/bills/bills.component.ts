9d7b3d892f4a35b34368137f551dcf6a2a50424e
Benjamin mitton <benjamin.mitton@avisto.com>
2025-09-05 17:09:51 +0200

    bills page added

--- src/app/pages/bills/bills.component.ts
+++ src/app/pages/bills/bills.component.ts
@@ -1,12 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HeaderImageComponent } from '../../core/components/header-image/header-image.component';
 
 @Component({
-  selector: 'app-bills',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './bills.component.html',
-  styleUrl: './bills.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-bills',
+    standalone: true,
+    imports: [CommonModule, HeaderImageComponent],
+    templateUrl: './bills.component.html',
+    styleUrl: './bills.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BillsComponent {}
+export class BillsComponent {
+    getRandomBills(): string {
+        return Array.from({ length: 6 }, () =>
+            String(Math.floor(Math.random() * 9) + 1),
+        ).join('');
+    }
+}
