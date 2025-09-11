e01c68749edb6de49a8fc799a84923773cd5d836
Julien BELTRAME <SESA726920@non.se.com>
2025-09-11 16:39:33 +0200

    wip
    Signed-off-by: Julien BELTRAME <SESA726920@non.se.com>

--- src/app/pages/location/location.component.ts
+++ src/app/pages/location/location.component.ts
@@ -1,14 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { HeaderImageComponent } from '../../core/components/header-image/header-image.component';
 import { RouterModule } from '@angular/router';
+import { Dialog } from '@angular/cdk/dialog';
+import { DessertModalComponent } from '../../modal/dessert-modal/dessert-modal.component';
+import { ItemSelectionComponent } from '../../core/components/item-selection/item-selection.component';
 
 @Component({
     selector: 'app-location',
     standalone: true,
-    imports: [CommonModule, RouterModule, HeaderImageComponent],
+    imports: [
+        CommonModule,
+        RouterModule,
+        HeaderImageComponent,
+        ItemSelectionComponent,
+    ],
     templateUrl: './location.component.html',
     styleUrl: './location.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LocationComponent {}
+export class LocationComponent {
+    dialog = inject(Dialog);
+    openModal() {
+        this.dialog.open(DessertModalComponent);
+    }
+}
