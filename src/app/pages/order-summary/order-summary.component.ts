33becc708b6d281e0465519e172c9b3025b72e8e
Julien BELTRAME <SESA726920@non.se.com>
2025-10-13 14:50:32 +0200

    remove debbuging code

--- src/app/pages/order-summary/order-summary.component.ts
+++ src/app/pages/order-summary/order-summary.component.ts
@@ -1,11 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { HeaderImageComponent } from '../../core/components/header-image/header-image.component';
-import { OrderService } from '../../core/services/order.service';
-import { MenuService } from '../../core/services/menu.service';
-import { ProductService } from '../../core/services/product.service';
-import { SelectionFooterComponent } from '../../core/components/selection-footer/selection-footer.component';
 import { Router } from '@angular/router';
+import { HeaderImageComponent } from '../../core/components/header-image/header-image.component';
+import { SelectionFooterComponent } from '../../core/components/selection-footer/selection-footer.component';
+import { OrderService } from '../../core/services/order.service';
 
 @Component({
     selector: 'app-order-summary',
@@ -18,20 +16,6 @@ import { Router } from '@angular/router';
 export class OrderSummaryComponent {
     router = inject(Router);
     orderService = inject(OrderService);
-    menuService = inject(MenuService);
-    productService = inject(ProductService);
-
-    constructor() {
-        const menu = this.menuService.getMenu('Menu Doggo');
-        const drink = this.productService.getDrinkProductList()[0];
-        const side = this.productService.getSideProductList()[0];
-        menu.changeDrink(drink);
-        menu.changeSide(side);
-        this.orderService.addMenu(menu);
-        this.orderService.addMenu(menu);
-        this.orderService.addProduct(drink);
-        this.orderService.addProduct(side);
-    }
 
     onConfirm(): void {
         this.router.navigate(['/payment']);
