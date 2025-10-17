8aeccd58c2b0cf21babc03694660b4ffdd2354a5
Julien BELTRAME <SESA726920@non.se.com>
2025-10-17 16:28:50 +0200

    Prepare Step2

--- src/app/pages/location/location.component.ts
+++ src/app/pages/location/location.component.ts
@@ -1,13 +1,11 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { HeaderImageComponent } from '../../core/components/header-image/header-image.component';
-import { OptionButtonComponent } from '../../core/components/option-button/option-button.component';
-import { OrderService } from '../../core/services/order.service';
 
 @Component({
     selector: 'app-location',
     standalone: true,
-    imports: [HeaderImageComponent, OptionButtonComponent],
+    imports: [HeaderImageComponent],
     templateUrl: './location.component.html',
     styleUrl: './location.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
