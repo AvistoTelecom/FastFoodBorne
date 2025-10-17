2990de47f1d361b1af89a61a202d684c68d5a249
Julien BELTRAME <SESA726920@non.se.com>
2025-10-17 14:29:09 +0200

    Prepare Exo7

--- src/app/pages/payment/payment.component.ts
+++ src/app/pages/payment/payment.component.ts
@@ -1,29 +1,21 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderImageComponent } from '../../core/components/header-image/header-image.component';
 import { SmallButtonComponent } from '../../core/components/small-button/small-button.component';
-import { OptionButtonComponent } from '../../core/components/option-button/option-button.component';
-import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-payment',
     standalone: true,
-    imports: [
-        HeaderImageComponent,
-        SmallButtonComponent,
-        OptionButtonComponent,
-    ],
+    imports: [HeaderImageComponent, SmallButtonComponent],
     templateUrl: './payment.component.html',
     styleUrl: './payment.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PaymentComponent {
-    router = inject(Router);
-
     toMenu(): void {
-        this.router.navigate(['/home']);
+        return;
     }
 
     toBill(): void {
-        this.router.navigate(['/bill']);
+        return;
     }
 }
