e1a0293e2064f500cfeabd9eb1680b78b1ff5a12
BenjamVisto <136731475+BenjamVisto@users.noreply.github.com>
2026-09-24 10:32:16 +0200

    ✨ zoneless mode (#9)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
     standalone: true,
     imports: [RouterOutlet],
     templateUrl: './app.component.html',
-    changeDetection: ChangeDetectionStrategy.Eager,
+    changeDetection: ChangeDetectionStrategy.OnPush,
     styleUrl: './app.component.css',
 })
 export class AppComponent {
